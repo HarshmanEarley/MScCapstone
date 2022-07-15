d5ab16cb29c6fe3d986aa3ac833a9e99aa7af9c6
denisor96 <90693325+denisor96@users.noreply.github.com>
2022-07-15 09:04:29 +0100

    Update config.R
    added paths

--- R/config.R
+++ R/config.R
@@ -1,10 +1,14 @@
 
 #Working directory
 PATH_WD = '/Users/root1/Documents/GitHub/Summer/Project/Code/'
+# DENIS PATH
+#PATH_WD = 'C:/Users/denis/Documents/GitHub/DAC_Project/R/'
 setwd(PATH_WD)
 
 #Database files
 PATH_DB = '/Users/root1/Documents/amex-default-prediction/'
+# DENIS PATH
+#PATH_DB = "C:/Users/denis/Documents/ACM40960 - Projects in Maths Modelling/database"
 FILES_DB = c('test_data','train_data','train_labels')
 
 #Create cache
