f6afeb1469114333f2e4971f59e592aab10a3088
denisor96 <90693325+denisor96@users.noreply.github.com>
2022-07-22 21:04:33 +0100

    update
    update

--- R/RandomForrest.R
+++ R/RandomForrest.R
@@ -8,10 +8,14 @@ library(keras)
 library(randomForest)
 
 
+###### sample set up of data ######
+# creates train, val and test data for models
+
+
 setwd("~/ACM40960 - Projects in Maths Modelling/database")
 # create data
-labels <- read.csv("train_labels.csv", header =T,nrows=2400)
-data <- read.csv("cleandata.csv", header =T, nrows=24000)
+labels <- read.csv("train_labels.csv", header =T,nrows=10000)
+data <- read.csv("cleandata.csv", header =T, nrows=50000)
 
 #merge
 data <- merge(data, labels, by = "customer_ID")
@@ -20,57 +24,66 @@ data <- merge(data, labels, by = "customer_ID")
 data <- date_clean(data)
 
 # Set NA's to 0
-is.na(data) %>% colSums() %>% sum()
 data[is.na(data)] <- 0
 
-colnames(data)
+# shuffle
+data <- data[sample(1:nrow(data)), ]
+
+# NOTE
+# subset to 50 columns for efficiency
+data <- data[, -c(50:166)]
 
 # create predict and target data
 pred <- data[ , -which(names(data) %in% c( 'customer_ID', 'target'))] 
 target <- as.factor(data$target)
 
-# subset to 50 columns for efficiency
-# REMOVE LATER
-pred  <- pred[, 1:50]
-
-
-###################### RF - tuning number of trees- 15 iterations - 30% validation data ######################
-
-B = 15
+# split the data linearly as it was shuffled/randomised above
 n <- nrow(pred)
-p <- nrow(pred)*(4/13)
+tr <- n*(.7) 
+va <- n*(.2)
+te <- n*.1
 
-tun_res <- matrix(0, nrow = B, ncol = 2)
+train <- 1:tr
+val <- tr:(tr+va)
+test <- (tr+va):n
+
+pred_train <- pred[train,]
+pred_val <- pred[val,]
+pred_test <- pred[test,]
+
+target_train <- target[train]
+target_val <- target[val]
+target_test <- target[test]
 
 
-for (i in 1:B){
-  print(i)
-  test <- sample(1:n, p, replace = FALSE)
-  train <- setdiff(1:n, test)
+###### tune number of trees ######
+# returns number of tree to minimize the error on validation data
+
+rf_tuning <- function( pred_train , target_train ,
+                       pred_val , target_val){
   
-  pred_test <- pred[test,]
-  pred_train <- pred[train,]
-  
-  target_test <- target[test]
-  target_train <- target[train]
-  
-  err_rate <-  randomForest(x = pred_train , y=target_train, 
-                            xtest = pred_test, ytest = target_test)$test$err.rate[,1]
-  tun_res[i,] <- c(which.min(err_rate), min(err_rate))
+  return( which.min(randomForest(x = pred_train , y=target_train, 
+                                 xtest = pred_val, ytest = target_val)$test$err.rate[,1] ) )
   
 }
 
-tun_res
-summary(tun_res[,1])
-# 250 - 350?
+ntrees <- rf_tuning( pred_train = pred_train, target_train = target_train,
+           pred_val = pred_val , target_val = target_val)
 
-num_tree = 350
+ntrees
 
-###################### simple RF - no test data ######################
+###### evaluate on test data ######
+# returns sensitivity, specificity and accuracy
 
-RF_simple <- function(pred = pred, target = target){
+rf_eval <- function( pred_train, target_train,
+                     pred_test, target_test,
+                     ntrees){
+  
   # run RF
-  conf <- randomForest(x = pred , y=target )$confusion
+  # test data confusion matrix
+  conf <- randomForest(x = pred_train , y=target_train,
+                       xtest = pred_test, ytest = target_test,
+                       ntree = as.integer(x = ntrees))$test$confusion
   # calculate sensitivity, specificity and accuracy
   # columns are predicted values
   # rows are observed values
@@ -80,79 +93,104 @@ RF_simple <- function(pred = pred, target = target){
   # return results as list
   results <- list(sens, spec, acc)
   names(results) = c("sens", "spec", "acc")
-  return(results)
+  
+  return( results)
+  
 }
 
-res <- RF_simple(pred = pred, target = target)
-res
+# evaluate using training data or training and validation?
+result <- rf_eval( pred_train = rbind(pred_train,pred_val), target_train = c(target_train,target_val),
+                   pred_test = pred_test , target_test = target_test,
+                   ntrees = ntrees)
+
+result
 
 
+###### loop over 500000 obs ######
 
-###################### RF - split data into random 30% test data ######################
+# column names for skipped rows etc.
+colnames_data <- colnames(read.csv("cleandata.csv", header =T, nrows=1))
+colnames_label <- colnames(read.csv("train_labels.csv", header =T, nrows=1))
 
-RF_test1 <- function(pred = pred, target = target){
+# labels
+labels <- read.csv("train_labels.csv", header =T,nrows=50000)
+
+# dataframe to store results
+df_result <- data.frame(matrix(0, nrow = 10, ncol = 4))
+colnames(df_result) <- c("ntrees","sens", "spec", "acc")
+
+# loop over 10 with 50000 obs
+for (i in 1:10){
+  
+  # create data
+  data <- read.csv("cleandata.csv", header =F, nrows=50000, skip = 1 + ((i-1)*50000))
+  colnames(data) <- colnames_data
+  
+  #merge
+  data <- merge(data, labels, by = "customer_ID")
+  
+  # Set NA's to 0
+  data[is.na(data)] <- 0
+  
+  # shuffle
+  data <- data[sample(1:nrow(data)), ]
+  
+  # subset to 50 columns for efficiency
+  data <- data[, -c(50:166)]
+  
+  # create predict and target data
+  pred <- data[ , -which(names(data) %in% c( 'customer_ID', 'target'))] 
+  target <- as.factor(data$target)
+  
   n <- nrow(pred)
-  p <- nrow(pred)*(4/13) # test proportion
-
-  test <- sample(1:n, p, replace = FALSE)
-  train <- setdiff(1:n, test)
+  tr <- n*(.7) 
+  va <- n*(.2)
+  te <- n*.1
   
-  pred_test <- pred[test,]
+  # split the data linearly as it was shuffled/randomised above
+  train <- 1:tr
+  val <- tr:(tr+va)
+  test <- (tr+va):n
+  
   pred_train <- pred[train,]
+  pred_val <- pred[val,]
+  pred_test <- pred[test,]
   
-  target_test <- target[test]
   target_train <- target[train]
+  target_val <- target[val]
+  target_test <- target[test]
   
-  # run RF
-  conf <- randomForest(x = pred_train , y=target_train, 
-                       xtest = pred_test, ytest = target_test)$test$confusion
-  # calculate sensitivity, specificity and overall accuracy
-  sens <- conf[2,2]/sum(conf[2,c(1,2)])
-  spec <- conf[1,1]/sum(conf[1,c(1,2)])
-  acc <- sum(diag(conf))/ sum(conf[c(1,2),c(1,2)])
-  # return results as list
-  results <- list(sens, spec, acc)
-  names(results) = c("sens", "spec", "acc")
-  return(results)
+  # get number of trees by tuning
+  ntrees <- rf_tuning( pred_train = pred_train, target_train = target_train,
+                       pred_val = pred_val , target_val = target_val)
+  
+  # evaluate on test data
+  result <- rf_eval( pred_train = rbind(pred_train,pred_val), target_train = c(target_train,target_val),
+                     pred_test = pred_test , target_test = target_test,
+                     ntrees = ntrees)
+  
+  # return number of trees and performance of model
+  df_result[i,] <- c(ntrees, result$sens, result$spec, result$acc)
+  
+  
+  cat(i, "\n")
 }
 
-res <- RF_test1(pred = pred, target = target)
-res
-
-###################### RF - data already split into test and train ######################
-
-# sample split
-n <- nrow(pred)
-p <- nrow(pred)*(4/13) 
-test <- sample(1:n, p, replace = FALSE)
-train <- setdiff(1:n, test)
-pred_test <- pred[test,]
-pred_train <- pred[train,]
-target_test <- target[test]
-target_train <- target[train]
+df_result
 
 
-RF_test2 <- function(pred_train, target_train, pred_test, target_test){
-  
-  # run RF
-  conf <- randomForest(x = pred_train , y=target_train, 
-                       xtest = pred_test, ytest = target_test)$test$confusion
-  # calculate sensitivity, specificity and overall accuracy
-  sens <- conf[2,2]/sum(conf[2,c(1,2)])
-  spec <- conf[1,1]/sum(conf[1,c(1,2)])
-  acc <- sum(diag(conf))/ sum(conf[c(1,2),c(1,2)])
-  # return results as list
-  results <- list(sens, spec, acc)
-  names(results) = c("sens", "spec", "acc")
-  return(results)
-}
-
-res <- RF_test2(pred_train = pred_train, target_train = target_train, pred_test = pred_test, target_test = target_test)
-res
-
-
-
-
+# sample results
+# ntrees      sens      spec       acc
+# 1     312 0.9196911 0.9643821 0.9528094
+# 2     305 0.8851133 0.9702523 0.9492102
+# 3     326 0.8982786 0.9653505 0.9482104
+# 4     430 0.8986750 0.9661108 0.9488102
+# 5     454 0.8841270 0.9716653 0.9496101
+# 6     350 0.8841310 0.9755906 0.9538092
+# 7     495 0.9029203 0.9638457 0.9484103
+# 8     108 0.8634868 0.9682959 0.9428114
+# 9     469 0.8956805 0.9753577 0.9558088
+# 10    445 0.8936170 0.9762154 0.9568086
 
 
 
