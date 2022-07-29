92213b842853c7994932a812f00376ce2d0d71a7
HarshmanEarley <sidney@harshmanearley.com>
2022-07-30 00:05:19 +0100

    update slash on path for convenience

--- R/config.R
+++ R/config.R
@@ -54,8 +54,8 @@ if(user == 'Sidney'){
     PATH_DB = '/Users/root1/Documents/amex-default-prediction/'
   }
   if(.Platform$OS.type == 'windows'){
-    PATH_WD = 'C:\\Users\\sidne\\Documents\\GitHub\\DAC_Project\\R\\'
-    PATH_DB = 'C:\\Users\\sidne\\Documents\\amexDatabase\\'
+    PATH_WD = 'C:/Users/sidne/Documents/GitHub/DAC_Project/R/'
+    PATH_DB = 'C:/Users/sidne/Documents/amexDatabase/'
   }
 }
 if(user == 'Denis'){
