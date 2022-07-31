fd3782e376583631247e34bbd7915b5d4cbf849f
HarshmanEarley <sidney@harshmanearley.com>
2022-07-31 16:30:51 +0100

    Create main.R as configuration script

---
+++ R/main.R
@@ -0,0 +1,17 @@
+## Welcome
+## Please specify a user
+user = 'Guest'
+
+## If guest, please specify directories below
+## NB - paths must end in a "/"
+##    - Windows directories must have either "\\" or "/" separators
+
+## Root working directory of Github clone
+PATH_WD = '/Users/root1/Documents/DAC_Project/'
+
+## Root database directory downloaded from Google Drive
+## https://drive.google.com/drive/u/0/folders/1C2TYJRsVH681dylc8ZX5A4gP-LsoaW7o
+PATH_DB = '/Users/root1/Documents/amex-default-prediction/'
+
+## Source database file to load all scripts
+source(paste(PATH_WD,"R/database.R", sep = ""))
